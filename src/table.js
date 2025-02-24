e788a875a96b59e24fd27cde024ead59e43170e5
Livenre <livenre@gmail.com>
2025-02-24 19:53:13 +0300

    commit4

--- src/table.js
+++ src/table.js
@@ -1,3 +1,111 @@
+const state = {
+  texts: ["тест", "foo"],
+  selected: [2, 3, 5, 7],
+  isAuthorMode: false,
+  currentlyEditing: null,
+};
+
+function winConditionMet() {
+  for (let i = 0; i < 5; i++) {
+    const selectedColumn = state.selected.filter((item) => item % 5 === i);
+    if (selectedColumn.length === 5) {
+      return true;
+    }
+    const selectedRow = state.selected.filter(
+      (item) => Math.floor(item / 5) === i,
+    );
+    if (selectedRow.length === 5) {
+      return true;
+    }
+  }
+
+  const mainDiagonal = state.selected.filter((item) => item % 6 === 0);
+  if (mainDiagonal.length === 5) {
+    return true;
+  }
+
+  const secondaryDiagonal = state.selected.filter((item) => item % 4 === 0);
+  if (secondaryDiagonal.length === 5) {
+    return true;
+  }
+
+  return false;
+}
+
+export function render(table) {
+  //console.log(structuredClone(state));
+  const tableElement = table ?? document.querySelector("table");
+  const cells = [...tableElement.querySelectorAll("td")];
+  state.texts.forEach((text, index) => {
+    cells[index].textContent = text ?? index;
+  });
+
+  cells.forEach((cell, idx) => {
+    if (state.selected.includes(idx)) {
+      cell.classList.add("selected");
+    } else {
+      cell.classList.remove("selected");
+    }
+
+    if (state.isAuthorMode && state.currentlyEditing === idx) {
+      alert("Тут має бути textarea");
+    }
+  });
+
+  const bingo = document.querySelector(".bingo");
+  if (winConditionMet()) {
+    //alert("win");
+    bingo.removeAttribute("hidden");
+    // ...
+  } else {
+    bingo.setAttribute("hidden", "hidden");
+    // ...
+  }
+}
+
+function handleCellClick(idx) {
+  if (state.isAuthorMode) {
+    state.currentlyEditing = idx;
+  } else {
+    if (!state.selected.includes(idx)) {
+      state.selected.push(idx);
+    } else {
+      state.selected = state.selected.filter((item) => item !== idx)
+    }
+  }
+
+  render();
+}
+
+export function generateTable(num){
+  const table = document.createElement("table");
+  const tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+  for (let i = 0; i < num; i++) {
+    const tr = document.createElement("tr");
+    for (let j = 0; j < num; j++) {
+      const td = document.createElement("td");
+      td.textContent = i * num + j;
+      td.addEventListener("click", () => handleCellClick(i * num + j));
+      tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+  } 
+
+  document
+    .querySelector('form[action="#new"]')
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+      state.selected.length = 0;
+      render();
+    });
+
+  
+  render(table);
+  return table;
+}
+
+/*
 function checkWin(table) {
   const rows = table.querySelectorAll("tr");
 
@@ -54,15 +162,7 @@ document.querySelector(".bingo form").addEventListener("submit", (e) => {
   resetBingo();
 });
 
-/*
-export function buttonRestart (id){
-  
-  const bingoelement = document.querySelector(".bingo");
-  bingoelement.setAttribute("hidden", true);
 
-}
-
-return table;*/
 
 function authorModeToggle() {
   if (document.querySelector(".switch input").checked) {
@@ -72,6 +172,7 @@ function authorModeToggle() {
 }
 
 document.querySelector(".switch input").addEventListener("change", authorModeToggle);
+
 
 
 
@@ -113,3 +214,4 @@ export function generateTable(num) {
 
   return table;
 }
+*/
